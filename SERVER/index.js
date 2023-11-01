126349e495aade0a6304dd0682b2514d11eff1df
Abhinav Singh <singh111abhinav@gmail.com>
2023-11-02 00:46:17 +0530

    tried few packages

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,6 +1,2 @@
-const http = require('http');
-
-http.createServer((req,res)=>{
-    res.write("<H1>This is the first web server</H1>")
-    res.end();
-}).listen(4000);
+const colors = require('colors');
+console.log("hello".green);
