08605bdf95a80b336d18d47342c1c99176de5955
Abhinav Singh <singh111abhinav@gmail.com>
2023-11-02 01:26:15 +0530

    simple api

---
+++ SIMPLE_API/data.js
@@ -0,0 +1,52 @@
+const data = {
+    "restaurantName": "Tasty Bites Restaurant",
+    "menu": [
+      {
+        "category": "Appetizers",
+        "items": [
+          {
+            "name": "Bruschetta",
+            "description": "Toasted baguette with diced tomatoes, basil, and garlic",
+            "price": 8.99
+          },
+          {
+            "name": "Spinach and Artichoke Dip",
+            "description": "Creamy spinach and artichoke dip with tortilla chips",
+            "price": 7.99
+          }
+        ]
+      },
+      {
+        "category": "Main Courses",
+        "items": [
+          {
+            "name": "Grilled Salmon",
+            "description": "Grilled salmon fillet with lemon butter sauce",
+            "price": 16.99
+          },
+          {
+            "name": "Chicken Alfredo",
+            "description": "Fettuccine pasta with grilled chicken in creamy Alfredo sauce",
+            "price": 14.99
+          }
+        ]
+      },
+      {
+        "category": "Desserts",
+        "items": [
+          {
+            "name": "Chocolate Lava Cake",
+            "description": "Warm chocolate cake with a molten center, topped with vanilla ice cream",
+            "price": 6.99
+          },
+          {
+            "name": "New York Cheesecake",
+            "description": "Classic New York-style cheesecake with berry compote",
+            "price": 5.99
+          }
+        ]
+      }
+    ]
+  }
+  
+module.exports=data;
